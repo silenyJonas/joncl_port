678a7ebab518683dc9cd21416c2cb27d992faea3
Jonáš Bučina <99987632+silenyJonas@users.noreply.github.com>
2025-04-14 13:11:30 +0200

    projects

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-projects',
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
   // kdyz se najede mysi tak se projekt rozbali dolu a ukaze se popis projektu
+  //bude tam  treba i link na github repo nebo treba pouzita technologie
+  projects: { title: string, backgroundPath: string, description: string , githubLink: string, githubLogo: string }[] = [
+    { title: 'Midnight Commander', backgroundPath: 'assets/projects-bg/mc.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' },
+    { title: 'Database Tool', backgroundPath: 'assets/projects-bg/db_tool.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' },
+    { title: 'Website NAUCSETO', backgroundPath: 'assets/projects-bg/mc.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' },
+    { title: 'Survey Solver', backgroundPath: 'assets/projects-bg/mc.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' }
+  ];
 }
