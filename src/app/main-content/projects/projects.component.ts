b9d0e15698d4e841243dc81fd75d7e06b6a0ba12
Jonáš Bučina <99987632+silenyJonas@users.noreply.github.com>
2025-04-14 19:52:00 +0200

    jj

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -10,9 +10,9 @@ export class ProjectsComponent {
   // kdyz se najede mysi tak se projekt rozbali dolu a ukaze se popis projektu
   //bude tam  treba i link na github repo nebo treba pouzita technologie
   projects: { title: string, backgroundPath: string, description: string , githubLink: string, githubLogo: string }[] = [
-    { title: 'Midnight Commander', backgroundPath: 'assets/projects-bg/mc.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' },
-    { title: 'Database Tool', backgroundPath: 'assets/projects-bg/db_tool.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' },
-    { title: 'Website NAUCSETO', backgroundPath: 'assets/projects-bg/mc.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' },
-    { title: 'Survey Solver', backgroundPath: 'assets/projects-bg/mc.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' }
+    { title: 'Desktop app', backgroundPath: 'assets/projects-bg/mc.png', description: '', githubLink: 'xxx', githubLogo: 'assets/images/github-projects.png' },
+    { title: 'Desktop app', backgroundPath: 'assets/projects-bg/db_tool.png', description: '', githubLink: 'xxx', githubLogo: 'assets/images/github-projects.png' },
+    { title: 'Website', backgroundPath: 'assets/projects-bg/web-bg.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' },
+    { title: 'Eshop', backgroundPath: 'assets/projects-bg/web-bg.png', description: '', githubLink: '', githubLogo: 'assets/images/github-projects.png' },
   ];
 }
